48d3921f128d8b86ed89c9ab7d5dfaa92a57d130
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2022-03-22 11:02:54 +0900

    ・金台にある癒しのプライベート空間の所の文字と画像のバランス調整 ・menuスライドの各アイテムを文字と画像で分けてコーディング（できれば端見切れるように） ・ご予約方法をスマホの時画像が上に来るようにする（flex-directionで調べてみて下さい）
    上記3点対応

--- js/slider.js
+++ js/slider.js
@@ -7,12 +7,16 @@ $('.slider').slick({
     slidesToScroll: 4,//1回のスクロールで4枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
     nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+    arrows: false,
+    adaptiveHeight: true,
+    centerMode: true,
     responsive: [
       {
         breakpoint: 769,//モニターの横幅が769px以下の見せ方
         settings: {
           slidesToShow: 1,//スライドを画面に1枚見せる
-          slidesToScroll: 4//1回のスクロールで4枚の写真を移動して見せる
+          slidesToScroll: 4,//1回のスクロールで4枚の写真を移動して見せる
+          centerMode: false,
         }
       },
     ]
